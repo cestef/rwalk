08825da90f8656ed55a177d55ec90cb1358ca183
cstef <colin@petit-suisse.fr>
2024-02-22 15:03:44 +0100

    Replace shaded block with space for progress bar

--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -13,4 +13,4 @@ pub const BANNER_STR: &str = r#"
 pub const SAVE_FILE: &str = ".rwalk.json";
 pub const STATUS_CODES: &str = "200-299,301,302,307,401,403,405,500";
 pub const PROGRESS_TEMPLATE: &str = "{spinner:.blue} (ETA. {eta}) {wide_bar} {pos}/{len} ({per_sec:>11}) | {prefix:>3} {msg:>14.bold}";
-pub const PROGRESS_CHARS: &str = "█▉▊▋▌▍▎▏░";
+pub const PROGRESS_CHARS: &str = "█▉▊▋▌▍▎▏ ";
