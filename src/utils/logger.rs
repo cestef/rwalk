9a90df6ed2720dd7cbbee62289d845d49c583235
cstef <colin@petit-suisse.fr>
2025-01-02 12:26:14 +0100

    fix: logger formatting for new env_logger version

--- src/utils/logger.rs
+++ src/utils/logger.rs
@@ -1,4 +1,4 @@
-use env_logger::{fmt::Color, Builder, Env};
+use env_logger::{fmt::style::AnsiColor as Color, Builder, Env};
 
 use std::io::Write;
 
@@ -22,31 +22,36 @@ pub fn init_logger() {
         .filter_module("rustyline::tty::unix", log::LevelFilter::Warn)
         .filter_module("rustyline::tty::unix::termios_", log::LevelFilter::Warn)
         .format(|buf, record| {
-            let mut style = buf.style();
-            match record.level() {
-                log::Level::Info => style.set_color(Color::Blue),
-                log::Level::Warn => style.set_color(Color::Yellow),
-                log::Level::Error => style.set_color(Color::Red),
-                log::Level::Debug => style.set_color(Color::Cyan),
-                log::Level::Trace => style.set_color(Color::Magenta),
-            };
+            let icon_style = env_logger::fmt::style::Style::new().fg_color(Some(
+                (match record.level() {
+                    log::Level::Info => Color::Blue,
+                    log::Level::Warn => Color::Yellow,
+                    log::Level::Error => Color::Red,
+                    log::Level::Debug => Color::Cyan,
+                    log::Level::Trace => Color::Magenta,
+                })
+                .into(),
+            ));
 
-            let icon = style.value(match record.level() {
+            let icon = match record.level() {
                 log::Level::Info => "ℹ",
                 log::Level::Warn => "⚠",
                 log::Level::Error => "✖",
                 log::Level::Debug => "⚙",
                 log::Level::Trace => "⚡",
-            });
+            };
+
             let module = match record.level() {
-                log::Level::Debug => Some(style.value(record.module_path().unwrap())),
-                log::Level::Trace => Some(style.value(record.module_path().unwrap())),
+                log::Level::Debug => Some(record.module_path().unwrap()),
+                log::Level::Trace => Some(record.module_path().unwrap()),
                 _ => None,
             };
 
+            let module_style = env_logger::fmt::style::Style::new().dimmed();
+
             writeln!(
                 buf,
-                "{} {}{}",
+                "{icon_style}{}{icon_style:#} {module_style}{}{module_style:#}{}",
                 icon,
                 if let Some(module) = module {
                     format!("({}) ", module)
