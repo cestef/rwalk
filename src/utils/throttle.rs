3051a8c10849503ba9be33a5463916b242714d6f
cstef <colin@petit-suisse.fr>
2025-02-16 13:43:54 +0100

    refactor: smoother semaphore permits adding

--- src/utils/throttle.rs
+++ src/utils/throttle.rs
@@ -1,5 +1,8 @@
 use std::{sync::Arc, time::Duration};
-use tokio::{sync::Semaphore, time::sleep};
+use tokio::{
+    sync::Semaphore,
+    time::{sleep, Instant},
+};
 
 pub struct SimpleThrottler {
     rps: u64,
@@ -10,14 +13,27 @@ impl SimpleThrottler {
     pub fn new(rps: u64) -> Self {
         let instance = Self {
             rps,
-            semaphore: Arc::new(Semaphore::new(rps as usize)),
+            semaphore: Arc::new(Semaphore::new(0)),
         };
-        // Spawn token replenishment task
+
         let sem = instance.semaphore.clone();
         tokio::spawn(async move {
+            let interval = Duration::from_secs_f64(1.0 / rps as f64);
+            let mut next_release = Instant::now();
+
             loop {
-                sleep(Duration::from_secs(1)).await;
-                sem.add_permits(rps as usize);
+                let now = Instant::now();
+                if next_release > now {
+                    sleep(next_release - now).await;
+                }
+
+                sem.add_permits(1);
+
+                next_release += interval;
+
+                if Instant::now() > next_release + Duration::from_secs(1) {
+                    next_release = Instant::now();
+                }
             }
         });
 
@@ -26,5 +42,34 @@ impl SimpleThrottler {
 
     pub async fn wait_for_request(&self) {
         self.semaphore.acquire().await.unwrap().forget();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::time::Instant;
+
+    #[tokio::test]
+    async fn test_smooth_throttler() {
+        let throttler = Arc::new(SimpleThrottler::new(10)); // 10 RPS
+        let start = Instant::now();
+        let mut tasks = Vec::new();
+        for _ in 0..10 {
+            tasks.push(tokio::spawn({
+                let throttler = throttler.clone();
+                async move {
+                    throttler.wait_for_request().await;
+                }
+            }));
+        }
+
+        for task in tasks {
+            task.await.unwrap();
+        }
+
+        let elapsed = start.elapsed();
+        println!("Elapsed: {:?}", elapsed);
+        assert!(elapsed < Duration::from_secs(1));
     }
 }
